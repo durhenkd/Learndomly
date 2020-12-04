6689bc6bbdce875e946da0903a161c774ad1a01b
durhenkd <gavrilatiberiu26@gmail.com>
2020-12-04 18:54:17 +0200

    incepem cu div-urile

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ window.addEventListener("keydown", checkKeyPress, false);
 function checkKeyPress(key){
     if(key.keyCode == "32")
         {   
-            var pool = YouTube;
+            for(let i=0; i<document.getElementsByClassName("screens").length; i++)
+                document.getElementsByClassName("screens")[i].style.display = "none";
             
+            
+            var pool = YouTube;
             
             let rIndex = Math.floor(Math.random() * pool.length);
             
+            document.getElementById("screenYT").style.display = "block";
             document.getElementById("screenYT").src =pool[rIndex].link;
-
         }
 }
 
